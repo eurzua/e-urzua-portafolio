6292c6951dd118892242afffca2cb05ee7b31741
Unknown <edu.urzua@gmail.com>
2018-06-24 06:16:57 -0400

    added new gallery and pictures for the same

--- gallery.php
+++ gallery.php
@@ -3,7 +3,7 @@
 <div class="container"> 
   <div class="row">
     <div class="main__header-container projects">
-      <p class="main__header-comments">“ Full stack no es saber de todo, si no saber como aplicar ese todo. “</p>
+      <p class="main__header-comments">“ Full stack no solo es saber de todo, si no es saber como aplicar ese todo. “</p>
       <span class="main__header-comments-name">Eduardo Urzúa</span>
       <h1 class="main__header-title">Projets</h1>
       <h2 class="main__header-subtitle">Diseñador, productor y desarrollador</h2>
@@ -11,190 +11,123 @@
   </div>
 </div>
 
-<main>
-		<div class="sections">
-			<!-- codrops links -->
-			<!-- facts expander with toggles; content for each is in its section -->
-			<div class="facts" style="display: none;"></div>
-			<!-- index -->
-			<div class="sections__index">
-				<span class="sections__index-current">
-					<span class="sections__index-inner">01</span>
-				</span>
-				<span class="sections__index-total">04</span>
+<svg class="hidden">
+			<symbol id="icon-arrow" viewBox="0 0 24 24">
+				<title>arrow</title>
+				<polygon points="6.3,12.8 20.9,12.8 20.9,11.2 6.3,11.2 10.2,7.2 9,6 3.1,12 9,18 10.2,16.8 "/>
+			</symbol>
+			<symbol id="icon-drop" viewBox="0 0 24 24">
+				<title>drop</title>
+				<path d="M12,21c-3.6,0-6.6-3-6.6-6.6C5.4,11,10.8,4,11.4,3.2C11.6,3.1,11.8,3,12,3s0.4,0.1,0.6,0.3c0.6,0.8,6.1,7.8,6.1,11.2C18.6,18.1,15.6,21,12,21zM12,4.8c-1.8,2.4-5.2,7.4-5.2,9.6c0,2.9,2.3,5.2,5.2,5.2s5.2-2.3,5.2-5.2C17.2,12.2,13.8,7.3,12,4.8z"/><path d="M12,18.2c-0.4,0-0.7-0.3-0.7-0.7s0.3-0.7,0.7-0.7c1.3,0,2.4-1.1,2.4-2.4c0-0.4,0.3-0.7,0.7-0.7c0.4,0,0.7,0.3,0.7,0.7C15.8,16.5,14.1,18.2,12,18.2z"/>
+			</symbol>
+			<symbol id="icon-cross" viewBox="0 0 24 24">
+				<title>cross</title>
+				<path d="M 5.5,2.5 C 5.372,2.5 5.244,2.549 5.146,2.646 L 2.646,5.146 C 2.451,5.341 2.451,5.659 2.646,5.854 L 8.793,12 2.646,18.15 C 2.451,18.34 2.451,18.66 2.646,18.85 L 5.146,21.35 C 5.341,21.55 5.659,21.55 5.854,21.35 L 12,15.21 18.15,21.35 C 18.24,21.45 18.37,21.5 18.5,21.5 18.63,21.5 18.76,21.45 18.85,21.35 L 21.35,18.85 C 21.55,18.66 21.55,18.34 21.35,18.15 L 15.21,12 21.35,5.854 C 21.55,5.658 21.55,5.342 21.35,5.146 L 18.85,2.646 C 18.66,2.451 18.34,2.451 18.15,2.646 L 12,8.793 5.854,2.646 C 5.756,2.549 5.628,2.5 5.5,2.5 Z"/>
+			<!-- Magnifier by Gregor Cresnar https://www.flaticon.com/authors/gregor-cresnar -->
+			<symbol id="icon-magnifier" viewBox="0 0 490.8 490.8">
+				<title>magnifier</title>
+				<path d="M364.8,299.55c46.3-75.8,36.9-176.3-28.6-241.9c-76.8-76.8-201.8-76.8-278.6,0s-76.8,201.8,0,278.5c65.5,65.5,166,74.9,241.9,28.6L412,477.25c18,18,47.3,18,65.3,0s18-47.3,0-65.3L364.8,299.55z M295.5,295.55c-54.4,54.4-142.8,54.4-197.1,0c-54.4-54.4-54.4-142.8,0-197.1c54.4-54.4,142.8-54.4,197.1,0C349.8,152.75,349.8,241.15,295.5,295.55z M220,171.95h59.4v45.3H220v59.4h-45.3v-59.4h-59.3v-45.3h59.4v-59.4h45.3v59.4H220z"/>
+			</symbol>
+		<main>
+			<div class="content">
+				<div class="grid">
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Marble Dream</h2>
+							<h3 class="product__subtitle">Constantin Frecker</h3>
+							<p class="product__description">Hashtag cred air plant drinking vinegar. Leggings yuccie chambray pop-up tousled hell of. Portland wolf mumblecore, synth cold-pressed polaroid poke cardigan gochujang farm-to-table photo booth.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Space Fantasy</h2>
+							<h3 class="product__subtitle">Danica Green</h3>
+							<p class="product__description">Man bun banjo pop-up meh hammock. Skateboard hammock tousled retro, etsy taiyaki narwhal gentrify fixie food truck microdosing sustainable dreamcatcher.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/project_modules/fs/587bae19408983.5730c40a66e66.jpg" alt="img 09"/>
+							<h2 class="product__title">La Vega</h2>
+							<h3 class="product__subtitle">Graphic desing</h3>
+							<p class="product__description">Identifica leyes de composición en función del comportamiento de los elementos visuales en una aplicación determinada, Identifica sistemas 
+                reticulares, grillas o mallas de construcción, como estructura de una composición en función a sus características y aplicaciones, Constrir retículas como elemento ordenador 
+                de una composición, en función de las características del contenido a diagramar, y organizar los elementos visuales, de acuerdo a los requerimientos.. </p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/project_modules/1400/9b372848174895.5894ad4f32115.png" alt="img 09"/>
+							<h2 class="product__title">Babysitter</h2>
+							<h3 class="product__subtitle">Web desing</h3>
+							<p class="product__description">Consistía en desarrollar el proceso completo de planificación, y desarrollo de los mockup y maqueta, como tambén 
+                comprobación de la navegación de elementos de interacción asociados. Considerando la re estructuración del sitio, tanto como a nivel gráfico como a nivel de estructura.
+                 Se debía contemplar la realización de la versión de escritorio y smartphone para la nueva propuesta de sitio, Realizando
+                  una Investigación cuantitativa y cualitativa sobre la competencia que considere las fortalezas y debilidades</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Disco Fever</h2>
+							<h3 class="product__subtitle">Alice Muller</h3>
+							<p class="product__description">Single-origin coffee air plant kitsch paleo iPhone vegan cold-pressed slow-carb cornhole dreamcatcher palo santo salvia lo-fi. </p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Little Boxes</h2>
+							<h3 class="product__subtitle">Xavier Brighton</h3>
+							<p class="product__description">Drinking vinegar lumbersexual 90's flexitarian. Live-edge man bun air plant XOXO. Master cleanse vaporware keffiyeh.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Fractal Love</h2>
+							<h3 class="product__subtitle">Walter Perry</h3>
+							<p class="product__description">Leggings green juice DIY, flannel tattooed selvage plaid yr sartorial chia. Scenester you probably haven't heard of them locavore.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Liquidia</h2>
+							<h3 class="product__subtitle">Lidia Greenwood</h3>
+							<p class="product__description">Helvetica la croix readymade, butcher viral pitchfork chillwave pork belly vaporware blue bottle iceland semiotics subway tile irony.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">The Buzz</h2>
+							<h3 class="product__subtitle">Sarah Grand</h3>
+							<p class="product__description">Celiac distillery man braid venmo, selfies you probably haven't heard of them tote bag forage fanny pack activated charcoal kale chips lo-fi before they sold out.</p>
+						</div>
+					</div>
+					<div class="grid__item">
+						<div class="product">
+							<div class="product__bg"></div>
+							<img class="product__img" src="https://mir-s3-cdn-cf.behance.net/projects/202/aca18854979705.Y3JvcCwxNjcyLDEzMDgsMCwxODE.jpg" alt="img 09"/>
+							<h2 class="product__title">Sweet Escape</h2>
+							<h3 class="product__subtitle">Peter Gradia</h3>
+							<p class="product__description">Mumblecore bespoke blog raw denim, authentic VHS sustainable +1 freegan neutra small batch paleo. Schlitz chicharrones pork belly palo santo.</p>
+						</div>
+					</div>
+				</div>
 			</div>
-			<!-- navigation down -->
-			<nav class="sections__nav">
-				<button class="sections__nav-item sections__nav-item--prev">
-					<svg class="icon icon--navup">
-						<use xlink:href="#icon-navup"></use>
-					</svg>
-				</button>
-				<button class="sections__nav-item sections__nav-item--next">
-					<svg class="icon icon--navdown">
-						<use xlink:href="#icon-navdown"></use>
-					</svg>
-				</button>
-			</nav>
-			<!-- sections -->
-			<section class="section section--current">
-				<div class="section__content">
-					<h2 class="section__title">ClubMetro</h2>
-					<p class="section__description">
-						<span class="section__description-inner">OBJETIVO | Crear y desarrollar un rediseño del Club Metro Se debía realizar un rediseño en mobile para el Club Metro,
-							donde organizará los diferentes elementos de la interfaz y hará una experiencia de usuario desde que comienza el sitio
-							hasta, finalizar su navegación por parte del usuario.
-						</span>
-					</p>
-				</div>
-				<div class="section__img">
-					<div class="section__img-inner" style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/fs/35833548559921.589c9a2af0b15.png)"></div>
-				</div>
-				<div class="section__more">
-					<div class="section__more-inner section__more-inner--bg1">
-						<span class="section__more-text">Quieres ver más?</span>
-						<a href="#" class="section__more-link">
-							<span class="section__more-linktext">Explore all hikes</span>
-							<svg class="icon icon--arrowlong">
-								<use xlink:href="#icon-arrowlong"></use>
-							</svg>
-						</a>
-					</div>
-				</div>
-				<div class="section__expander"></div>
-				<ul class="section__facts">
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Cardsorting abierto.</h3>
-						<span class="section__facts-detail">Benchmark</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Mapa de Garret</h3>
-						<span class="section__facts-detail">Test heuristico</span>
-					</li>
-				</ul>
-			</section>
-			<!--/ section -->
-			<section class="section">
-				<div class="section__content">
-					<h2 class="section__title">Latam</h2>
-					<p class="section__description">
-						<span class="section__description-inner">Prototipo de flujo de compra de pasajes en mobile, para de esta forma mejorar la experiencia del flujo de manera eficaz
-							y al usuario generar una grata y rapida compra.</span>
-					</p>
-				</div>
-				<div class="section__img">
-					<div class="section__img-inner" style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/max_1200/1926f948129537.5894fc8422706.gif)"></div>
-				</div>
-				<div class="section__more">
-					<div class="section__more-inner section__more-inner--bg2">
-						<span class="section__more-text">Quieres ver más?</span>
-						<a href="https://www.behance.net/eurzua" class="section__more-link">
-							<span class="section__more-linktext">Ir a behance</span>
-							<svg class="icon icon--arrowlong">
-								<use xlink:href="#icon-arrowlong"></use>
-							</svg>
-						</a>
-					</div>
-				</div>
-				<div class="section__expander"></div>
-				<ul class="section__facts">
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">UX</h3>
-						<span class="section__facts-detail">Investigación tanto de flijo como de competencias</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">UI</h3>
-						<span class="section__facts-detail">Propotipado del flujo</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Rediseño</h3>
-						<span class="section__facts-detail">With 55m it's the deepest</span>
-					</li>
-				</ul>
-			</section>
-			<!--/ section -->
-			<section class="section">
-				<div class="section__content">
-					<h2 class="section__title">Turbus</h2>
-					<p class="section__description">
-						<span class="section__description-inner">Realización de una solución de compra de pasajes de la empresa TurBus y realizar el rediseño del sitio web, enfatizando
-							la experiencia de usuario y creando experiencia desde el primer momento de visitar la web, gracias a distinto procesos
-							realizados.
-						</span>
-					</p>
-				</div>
-				<div class="section__img">
-					<div class="section__img-inner" style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/2800/fc81fd47964209.5899ec3e158c6.png)"></div>
-				</div>
-				<div class="section__more">
-					<div class="section__more-inner section__more-inner--bg3">
-						<span class="section__more-text">Quieres ver más?</span>
-						<a href="https://www.behance.net/eurzua" class="section__more-link">
-							<span class="section__more-linktext">Ir a behance</span>
-							<svg class="icon icon--arrowlong">
-								<use xlink:href="#icon-arrowlong"></use>
-							</svg>
-						</a>
-					</div>
-				</div>
-				<div class="section__expander"></div>
-				<ul class="section__facts">
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Benchmark</h3>
-						<span class="section__facts-detail">Test heuristico</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Card sorting abuerto</h3>
-						<span class="section__facts-detail">Mapa de flujo con niveles y subniveles</span>
-					</li>
-				</ul>
-			</section>
-			<!--/ section -->
-			<section class="section">
-				<div class="section__content">
-					<h2 class="section__title">Wave Music</h2>
-					<p class="section__description">
-						<span class="section__description-inner">Propotipo de portal de venta y streaming de música electronica, realización de proyecto personal, para de esta forma
-							aplicar dinstos elementos aprendidos en la carrera.</span>
-					</p>
-				</div>
-				<div class="section__img">
-					<div class="section__img-inner" style="background-image: url(https://mir-s3-cdn-cf.behance.net/project_modules/max_3840/b564b154979705.5972607edc644.jpg)"></div>
-				</div>
-				<div class="section__more">
-					<div class="section__more-inner section__more-inner--bg4">
-						<span class="section__more-text">Quieres ver más?</span>
-						<a href="https://www.behance.net/eurzua" class="section__more-link">
-							<span class="section__more-linktext">Ir a behance</span>
-							<svg class="icon icon--arrowlong">
-								<use xlink:href="#icon-arrowlong"></use>
-							</svg>
-						</a>
-					</div>
-				</div>
-				<div class="section__expander"></div>
-				<ul class="section__facts">
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">River romantics</h3>
-						<span class="section__facts-detail">Caspian sea in Kazakhstan</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">History call</h3>
-						<span class="section__facts-detail">Kayaking the whole lake</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">Walking around</h3>
-						<span class="section__facts-detail">With 55m it's the deepest</span>
-					</li>
-					<li class="section__facts-item">
-						<h3 class="section__facts-title">How many miles?</h3>
-						<span class="section__facts-detail">An aerial view of the lake</span>
-					</li>
-				</ul>
-			</section>
-			<!--/ section -->
-		</div>
-		<!--/ sections -->
-	</main>
+		</main>
 
-  <?php include("footer.php"); ?>
+<?php include("footer.php"); ?>
