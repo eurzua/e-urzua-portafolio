977eeb0cdd8958555075c7a31d3b17a62cfc25fd
Unknown <edu.urzua@gmail.com>
2018-06-27 01:16:29 -0400

    changed buttom for a and added styles for responsive

--- index.php
+++ index.php
@@ -19,79 +19,41 @@
 <?php include("assets/_data/_services-mobile.php");?>
 
    <div class="row services__desktop">
-
-    <div class="col-md-4 col-sm-12">
-      <div class="card web">
-        <div class="card-body">
-          <span class="card__detail"></span>
-          <h4 class="card__title">Diseño Web</h4>
-          <h5 class="card__title-primary">Web Designer</h5>
-          <div class="card-img">Diseño web / Web Designer.</div>
-          <button data-toggle="collapse" data-target="#_contact" class="card-link">Contact us</button>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4 col-sm-12">
-      <div class="card development">
-        <div class="card-body">
-          <span class="card__detail"></span>
-          <h4 class="card__title">Desarrollo</h4>
-          <h5 class="card__title-primary">Development</h5>
-          <div class="card-img">Desarrollo / Development.</div>
-          <button data-toggle="collapse" data-target="#_contact" class="card-link">Contact us</button>
-        </div>
-      </div>
-    </div>
-
-    <div class="col-md-4 col-sm-12">
-      <div class="card graphic">
-        <div class="card-body">
-          <span class="card__detail"></span>
-          <h4 class="card__title">Diseño Gráfico</h4>
-          <h5 class="card__title-primary">Graphic design</h5>
-          <div class="card-img">Diseño gráfico / Graphic design.</div>
-          <button data-toggle="collapse" data-target="#_contact" class="card-link">Contact us</button>
-        </div>
-      </div>
-    </div>
-  </div>
-
-  <div class="row">
-    <div id="_contact" class="collapse">
-      <div class="row">
-          <div class=" col-12 ">
-              <form method="post" action="_contact.php">
-                  <div class="col-sm-12 form-group">
-                    <label for="nombre">Nombre</label>
-                      <input class="form-control" name="nombre" placeholder="nombre" type="text">
-                  </div>
-                  <div class="col-sm-12 form-group">
-                    <label for="telefono">Teléfono:</label>
-                      <input name="telefono" class="form-control" type="tel" placeholder="Teléfono">
-                  </div>
-                  <div class="col-sm-12 form-group">
-                    <label for="email">Email:</label>
-                      <input class="form-control"  name="email" placeholder="Email" type="email">
-                  </div>
-                  <div class="col-sm-12 form-group">
-                    <label for="asunto">Asunto:</label>
-                      <input class="form-control"  name="asunto" placeholder="asunto" type="text">
-                  </div>
-                  <div class="col-sm-12 form-group">
-                    <label for="telefono">Mensaje:</label>
-                      <textarea class="form-control" rows="5" name="cuerpo"></textarea>
-                  </div>
-                <div class="form-group text-center">
-                      <input type="submit" class="form-btn"  name="registrarse" onsubmit="">
-                  <div>
-              </form>
+      <div class="col-md-4 col-sm-12">
+        <div class="card web">
+          <div class="card-body">
+            <span class="card__detail"></span>
+            <h4 class="card__title">Diseño Web</h4>
+            <h5 class="card__title-primary">Web Designer</h5>
+            <div class="card-img">Diseño web / Web Designer.</div>
+            <a href="contact.php" alt="to contact" class="card-link">Contact us</a>
           </div>
+        </div>
       </div>
-</div>
-
+      <div class="col-md-4 col-sm-12">
+        <div class="card development">
+          <div class="card-body">
+            <span class="card__detail"></span>
+            <h4 class="card__title">Desarrollo</h4>
+            <h5 class="card__title-primary">Development</h5>
+            <div class="card-img">Desarrollo / Development.</div>
+            <a href="contact.php" alt="to contact" class="card-link">Contact us</a>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-4 col-sm-12">
+        <div class="card graphic">
+          <div class="card-body">
+            <span class="card__detail"></span>
+            <h4 class="card__title">Diseño Gráfico</h4>
+            <h5 class="card__title-primary">Graphic design</h5>
+            <div class="card-img">Diseño gráfico / Graphic design.</div>
+            <a href="contact.php" alt="to contact" class="card-link">Contact us</a>
+          </div>
+        </div>
+      </div>
+    </div>
   </div>
-
 </div>
 
 
@@ -100,10 +62,10 @@
    <h2 class="main__title">Projects</h2>
    <h3 class="main__subtitle">Projectos</h3>
     <div class="row">
-      <div class="col-md-6 col-xs-12 bg-warning d-flex align-self-center"  style="height: 526px; width: auto;">
+      <div class="col-md-6 col-sm-12 bg-warning d-flex align-self-center"  style="height: 526px; width: 100%; margin: 0 -15px">
         cualqueir cosa
       </div>
-      <div class="col-md-6 col-xs-12">
+      <div class="col-md-6 col-sm-12">
         <div class="col-12  bg-success" style="height: 526px; width: auto;">
         
         blabla
