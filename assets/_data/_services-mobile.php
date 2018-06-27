dac277512b5c4dbcd602cb8831367b8bd1d7bce7
Unknown <edu.urzua@gmail.com>
2018-06-26 20:51:18 -0400

    changed the id name for a better reference to the section

--- assets/_data/_services-mobile.php
+++ assets/_data/_services-mobile.php
@@ -1,10 +1,10 @@
-<div id="demo" class="carousel slide services-mobile" data-ride="carousel">
+<div id="services" class="carousel slide services-mobile" data-ride="carousel">
 
   <!-- Indicators -->
   <ul class="carousel-indicators">
-    <li data-target="#demo" data-slide-to="0" class="active"></li>
-    <li data-target="#demo" data-slide-to="1"></li>
-    <li data-target="#demo" data-slide-to="2"></li>
+    <li data-target="#services" data-slide-to="0" class="active"></li>
+    <li data-target="#services" data-slide-to="1"></li>
+    <li data-target="#services" data-slide-to="2"></li>
   </ul>
 
   <!-- The slideshow -->
@@ -51,10 +51,10 @@
   </div>
 
   <!-- Left and right controls -->
-  <a class="carousel-control-prev" href="#demo" data-slide="prev">
+  <a class="carousel-control-prev" href="#services" data-slide="prev">
     <span class="carousel-control-prev-icon"></span>
   </a>
-  <a class="carousel-control-next" href="#demo" data-slide="next">
+  <a class="carousel-control-next" href="#services" data-slide="next">
     <span class="carousel-control-next-icon"></span>
   </a>
 
