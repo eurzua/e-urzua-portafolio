fb2cc845dc404429641e946c74d139b157cd61d1
Unknown <edu.urzua@gmail.com>
2018-06-29 00:16:45 -0400

    fix main .js and added sweet alert

--- footer.php
+++ footer.php
@@ -11,9 +11,11 @@
     </footer>
     <script src='https://cdnjs.cloudflare.com/ajax/libs/jquery/3.3.1/jquery.js'></script>
 		<script src="js/imagesloaded.pkgd.min.js"></script>
-		<script src="js/anime.min.js"></script>
+    <script src="js/anime.min.js"></script>
+    <script src="js/main.js"></script>
     <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
      <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" integrity="sha384-smHYKdLADwkXOn1EmN1qk/HfnUcbVRZyYmZ4qpPea6sjB/pTJ0euyQp0Mk8ck+5T" crossorigin="anonymous"></script>
+     <script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
      <Script>
               //navbar responsive
 
@@ -37,5 +39,28 @@
                     document.body.classList.remove('loading'); 
                   }); 
     </Script>
+
+    <script type="text/javascript">
+
+          $(document).ready(function(){
+            $('#contact-form').on('submit',function(e) {  //Don't foget to change the id form
+            $.ajax({
+                url:'contact.php', //===PHP file name====
+                data:$(this).serialize(),
+                type:'POST',
+                success:function(data){
+                  console.log(data);
+                  //Success Message == 'Title', 'Message body', Last one leave as it is
+                swal("¡Excelente!", "Pronto nos comunicaremos contigo!", "success");
+                },
+                error:function(data){
+                  //Error Message == 'Title', 'Message body', Last one leave as it is
+                swal("Oops...", "Algo paso, por favor intenta otra vez :(", "error");
+                }
+              });
+              e.preventDefault(); //This is to Avoid Page Refresh and Fire the Event "Click"
+            });
+          });
+    </script>
   </body>
 </html>
